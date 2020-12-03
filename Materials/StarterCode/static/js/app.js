3af144e70c306b71eb91cbee1763480bffaa74ce
abigailrgreene <abigailrgreene@gmail.com>
2020-12-03 06:21:48 -0500

    bar chart and dropdown started

--- Materials/StarterCode/static/js/app.js
+++ Materials/StarterCode/static/js/app.js
@@ -1,9 +1,51 @@
+d3.json("samples.json").then(function(belly) {
+    var belly = belly.samples;
+    console.log(belly);
 
-// Fetch the JSON data and console log it
-d3.json("./samples.json").then(function(data) {
-    console.log("hello");
-    console.log(data);
+});
+
+function dripdrop() {
+  var picky = d3.select("#subjectHere");
+  d3.json("samples.json").then((belly) => {
+    var name = belly.names;
+
+    var patientzero = name[0];
+    builder(patientzero);
   });
-  
+}
 
-  
+
+
+dripdrop();
+
+
+function builder(sample) {
+  d3.json("samples.json").then((belly) => {
+    var samples = belly.samples;
+    var filtered = samples.filter(thing=> thing.id == sample);
+    var yolo = filtered[0];
+
+    var otu_ids = yolo.otu_ids;
+    var otu_labels = yolo.otu_labels;
+    var sample_values = yolo.sample_values;
+
+    var yticks = otu_ids.map(otuID => `OTU ${otuID}`).reverse();
+    var babar = [
+      {
+        y: yticks,
+        x: sample_values.reverse(),
+        text: otu_labels.reverse(),
+        type: "bar",
+        orientation: "h",
+      }
+    ];
+
+    var theElephant = {
+      title: "Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
+    };
+
+    Plotly.newPlot("bar", babar, theElephant);
+  });
+}
+
